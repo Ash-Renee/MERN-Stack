ca01f8f6e64adcfb4a3d10a42f0824eae129647b
Ashley D'Allessandro <ashley.dallessandro@gmail.com>
2021-05-06 19:05:35 -0700

    putting it together assignment

---
+++ putting_it_together/prop_it_up/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import BirthdayButton from './components/BirthdayButton';
+
+function App() {
+  return (
+    <div className="App">
+      <BirthdayButton firstName="Ashley" lastName="Renee" age={37} hairColor="pink"/>
+      <BirthdayButton/>
+      <BirthdayButton firstName="Chazz" lastName="Cupcake" age={31} hairColor="chestnut"/>
+      <BirthdayButton firstName="Clara" lastName="Smith" age={37} hairColor="blonde"/>
+      <BirthdayButton firstName="Yuri" lastName="Pan" age={32} hairColor="blue"/>
+    </div>
+  );
+}
+
+export default App;
