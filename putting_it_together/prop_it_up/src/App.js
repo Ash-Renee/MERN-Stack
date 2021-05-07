f7efaf39bb0de5bf1e5d6bc6c876ae0dac836710
Ashley D'Allessandro <ashley.dallessandro@gmail.com>
2021-05-07 13:40:40 -0700

    modfied to user person card for big inversion assn

--- putting_it_together/prop_it_up/src/App.js
+++ putting_it_together/prop_it_up/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
 
-import BirthdayButton from './components/BirthdayButton';
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <BirthdayButton firstName="Ashley" lastName="Renee" age={37} hairColor="pink"/>
+      <PersonCard firstName="Ashley" lastName="Renee" age={37} hairColor="pink"/>
 
-      <BirthdayButton firstName="Chazz" lastName="Cupcake" age={31} hairColor="chestnut"/>
-      <BirthdayButton firstName="Clara" lastName="Smith" age={37} hairColor="blonde"/>
-      <BirthdayButton firstName="Yuri" lastName="Pan" age={32} hairColor="blue"/>
+      <PersonCard firstName="Chazz" lastName="Cupcake" age={31} hairColor="chestnut"/>
+      <PersonCard firstName="Clara" lastName="Smith" age={37} hairColor="blonde"/>
+      <PersonCard firstName="Yuri" lastName="Pan" age={32} hairColor="blue"/>
     </div>
   );
 }
--- putting_it_together/prop_it_up/src/components/BirthdayButton.js
+++ putting_it_together/prop_it_up/src/components/BirthdayButton.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class BirthdayButton extends Component {
-    constructor(props){
-        super(props); //class based componets 'props' is the prperty, can be whatever, it's a variable/object
-        this.state = {
-            clickAge: this.props.age,
-            // resetAge: this.props.age, was not used
-        }
-        console.log(props);
-    }
+// class BirthdayButton extends Component {
+//     constructor(props){
+//         super(props); //class based componets 'props' is the prperty, can be whatever, it's a variable/object
+//         this.state = {
+//             clickAge: this.props.age,
+//             // resetAge: this.props.age, was not used
+//         }
+//         console.log(props);
+//     }
 
 
-    BirthdayClick = () => {
-        this.setState({ clickAge: this.state.clickAge +1})
-        console.log(this.state.clickAge);
-    }
+//     BirthdayClick = () => {
+//         this.setState({ clickAge: this.state.clickAge +1})
+//         console.log(this.state.clickAge);
+//     }
     
-    ResetClick = () => {
-        this.setState({clickAge: this.props.age})
-    }
-//state ccan be modified not props
-    render() {
-        const {firstName, lastName, hairColor} = this.props;
-        return (
-            <div>
-                <h1>{lastName}, {firstName}</h1>
-                <p>Age: {this.state.clickAge}</p>
-                <p>Hair Color: {hairColor}</p>
-                <button onClick={this.BirthdayClick}>Birthday button for {firstName} {lastName}</button>
-                <br />
-                <button onClick={this.ResetClick}>Reset Me!</button>
-            </div>
-        )
-    }
-}
-export default BirthdayButton;
+//     ResetClick = () => {
+//         this.setState({clickAge: this.props.age})
+//     }
+// //state ccan be modified not props
+//     render() {
+//         const {firstName, lastName, hairColor} = this.props;
+//         return (
+//             <div>
+//                 <h1>{lastName}, {firstName}</h1>
+//                 <p>Age: {this.state.clickAge}</p>
+//                 <p>Hair Color: {hairColor}</p>
+//                 <button onClick={this.BirthdayClick}>Birthday button for {firstName} {lastName}</button>
+//                 <br />
+//                 <button onClick={this.ResetClick}>Reset Me!</button>
+//             </div>
+//         )
+//     }
+// }
+// export default BirthdayButton;
